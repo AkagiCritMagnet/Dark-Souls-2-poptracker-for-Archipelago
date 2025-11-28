5e121301971ba7b1e5f780bfa39ce9bd9e1a6af8
AkagiCritMagnet <vivaldi_du_14@hotmail.com>
2025-11-28 22:05:07 +0100

    first commit

---
+++ scripts/logic.lua
@@ -0,0 +1,26 @@
+function has(item, amount)
+    local count = Tracker:ProviderCountForCode(item)
+    if not amount then
+        return count > 0
+    else
+        amount = tonumber(amount)
+        return count >= amount
+    end
+end
+
+function hasnot(item)
+	local count2 = Tracker:ProviderCountForCode(item)
+    if count2 > 0 then
+		return false
+	else return true
+	end
+end
+
+
+function canReduceFallDamage()
+    return has("Flying Feline Boots") or has("Silvercat Ring")
+end
+
+function canAccessPit()
+    return canReduceFallDamage() or has("Ladder Miniature")
+end
